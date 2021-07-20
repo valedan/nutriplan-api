7009b829025a72e5d841a5800970bbe66147a212
Dan <dan@danmolloy.dev>
2021-07-20 08:16:11 -0400

    apollo cors

--- src/index.js
+++ src/index.js
@@ -42,7 +42,7 @@ const resolvers = {
 
 // The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({ cors: true, typeDefs, resolvers });
 
 // The `listen` method launches a web server.
 server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
