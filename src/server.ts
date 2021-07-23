4df8f5dc2f9c9578f047b1fb1f892cad9ae8372a
Dan <dan@danmolloy.dev>
2021-07-23 08:24:43 -0400

    test

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,5 @@ import { ApolloServer } from "apollo-server";
 import { context } from "./context";
 import { schema } from "./schema";
 
+console.log("test");
 export const server = new ApolloServer({ schema, context });
