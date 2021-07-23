aa240c565a6ead26e9d6ff86ad945b00dfc8ec61
Dan <dan@danmolloy.dev>
2021-07-23 09:08:50 -0400

    ignore jest tests in dist folder

--- src/server.ts
+++ src/server.ts
@@ -2,5 +2,4 @@ import { ApolloServer } from "apollo-server";
 import { context } from "./context";
 import { schema } from "./schema";
 
-console.log("test");
 export const server = new ApolloServer({ schema, context });
