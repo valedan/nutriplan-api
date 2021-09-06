af5c52ccc6a5ddb53d06c113ea427b4eb54cfe3d
Dan <dan@danmolloy.dev>
2021-09-06 11:28:30 -0700

    add prod client origin to cors policy

--- src/config/server.ts
+++ src/config/server.ts
@@ -1,5 +1,9 @@
-import { ApolloServer } from "apollo-server";
-import { context } from "./context";
-import schema from "./schema";
+import { ApolloServer } from "apollo-server"
+import { context } from "./context"
+import schema from "./schema"
 
-export default new ApolloServer({ schema, context });
+export default new ApolloServer({
+  schema,
+  context,
+  cors: { origin: ["http://localhost:3000", "https://nutriplan.vercel.app"] },
+})
