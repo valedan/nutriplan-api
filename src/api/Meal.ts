aba54b67185cc96cb6511f9659e2ec25ee3c58ac
Dan Valentine <danvalentine256@gmail.com>
2022-02-02 09:01:39 -0500

    Add meals to plans

--- src/api/Meal.ts
+++ src/api/Meal.ts
@@ -1,8 +1,103 @@
 // input will not include ings - create them from recipe
 // addMeal
 
-// also delete ingredients, and update order of other meals/ings
+// also delete ings, and update order of other meals/ings
 // removeMeal
 
 // scalars only, excluding order
 // updateMeal
+import {
+  nonNull,
+  intArg,
+  list,
+  mutationField,
+  inputObjectType,
+  objectType,
+} from "nexus"
+import MealService from "../services/meal"
+
+export const addMealInput = inputObjectType({
+  name: "AddMealInput",
+  definition(t) {
+    t.nonNull.int("planId")
+    t.nonNull.int("recipeId")
+  },
+})
+
+export const updateMealInput = inputObjectType({
+  name: "UpdateMealInput",
+  definition(t) {
+    t.nonNull.int("id")
+    t.int("servings")
+  },
+})
+
+// TODO Make this work together with ingredients
+export const MealReorder = inputObjectType({
+  name: "MealReorder",
+  definition(t) {
+    t.nonNull.int("id")
+    t.nonNull.int("newOrder")
+  },
+})
+
+export const reorderMealsInput = inputObjectType({
+  name: "ReorderMealsInput",
+  definition(t) {
+    t.nonNull.int("parentId")
+    t.nonNull.list.field("reorders", { type: nonNull("MealReorder") })
+  },
+})
+
+export const addMeal = mutationField("addMeal", {
+  type: "Meal",
+  args: { input: nonNull(addMealInput) },
+  resolve: (_, { input }, ctx) => MealService.addMeal(input, ctx),
+})
+
+export const removeMeal = mutationField("removeMeal", {
+  type: "Meal",
+  args: { id: nonNull(intArg()) },
+  resolve: async (_, { id }, ctx) => MealService.removeMeal(id, ctx),
+})
+
+export const updateMeal = mutationField("updateMeal", {
+  type: "Meal",
+  args: { input: nonNull(updateMealInput) },
+  resolve: async (_, { input }, ctx) => MealService.updateMeal(input, ctx),
+})
+
+export const reorderMeals = mutationField("reorderMeals", {
+  type: list("Meal"),
+  args: { input: nonNull(reorderMealsInput) },
+  resolve: async (_, { input }, ctx) => MealService.reorderMeals(input, ctx),
+})
+
+// TODO: Ungroup meal
+
+export const Meal = objectType({
+  name: "Meal",
+  definition(t) {
+    t.nonNull.int("id")
+    t.nonNull.int("servings")
+    t.nonNull.int("order")
+
+    t.field("ingredients", {
+      type: nonNull(list(nonNull("Ingredient"))),
+      resolve: ({ id }, _args, ctx) =>
+        ctx.db.meal.findUnique({ where: { id } }).ingredients(),
+    })
+
+    t.field("recipe", {
+      type: "Recipe",
+      resolve: ({ id }, _args, ctx) =>
+        ctx.db.meal.findUnique({ where: { id } }).recipe(),
+    })
+
+    t.field("plan", {
+      type: "Plan",
+      resolve: ({ id }, _args, ctx) =>
+        ctx.db.meal.findUnique({ where: { id } }).plan(),
+    })
+  },
+})
---
+++ src/services/meal/index.ts
@@ -0,0 +1,9 @@
+import addMeal from "./addMeal"
+import removeMeal from "./removeMeal"
+import updateMeal from "./updateMeal"
+
+export default {
+  addMeal,
+  removeMeal,
+  updateMeal,
+}
